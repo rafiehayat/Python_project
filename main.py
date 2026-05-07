0f8a2027ee461940d56c4e62f0393ef8db7912ab
rafiehayat <rafiehayat5@gmail.com>
2026-05-07 21:22:30 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+print("hello World")
+
+str = "Hello Raife"
+
+a,b,c,d = 5,6.4,"Hayat",'Hayat'
+print(type(b))
+print("Name is " + c)
+print("{}{}".format("Value is " , a))
+print(type(a))
+
+
+age,height,favorite_color = 25,5.9,"blue"
+print(age)
+print(type(age))
+print(height)
+print(type(height))
+print(favorite_color)
+print(type(favorite_color))
