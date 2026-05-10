80e9a853268a288834653c978d4fcce707261fc7
rafiehayat <rafiehayat5@gmail.com>
2026-05-10 12:39:52 +0530

    loops

---
+++ loops.py
@@ -0,0 +1,24 @@
+greeting = "Good Morning"
+
+if greeting == "Morning":
+    print("Condition matches")
+    print("Second line")
+else:
+    print("Condition not match")
+print("if else condition completed")
+
+obj = [2,3,5,7,9]
+for i in obj:
+    print(i*2)
+
+summation=0
+for j in range(1,6):
+    summation = summation+j
+    print(j)
+print(summation)
+
+for k in range(1,10,3):
+    print(k)
+
+for l in range(10):
+    print(l)
