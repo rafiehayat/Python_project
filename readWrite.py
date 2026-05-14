862baf1b9fe510a3ffca8dc2abd6543632b9c31b
rafiehayat <rafiehayat5@gmail.com>
2026-05-14 21:41:26 +0530

    readWrite

---
+++ readWrite.py
@@ -0,0 +1,21 @@
+file = open('test.txt')
+
+# print(file.read(7))
+
+# print(file.readline())
+# print(file.readline())
+# print(file.readline())
+
+# line = file.readline()
+# while line != "":
+#     print(line)
+#     line=file.readline()
+
+# print(file.readlines())
+
+for line in file.readlines():
+    print(line)
+
+
+
+file.close()
