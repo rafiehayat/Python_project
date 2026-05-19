472f8a1e9fa60832000838ae18cd182dcfa59985
rafiehayat <rafiehayat5@gmail.com>
2026-05-19 12:26:39 +0530

    tryexcept

--- ExceptionPython.py
+++ ExceptionPython.py
@@ -6,3 +6,16 @@ if ItemsInCart!=2:
 
 
 assert(ItemsInCart==0)
+
+
+try:
+    with open("file.txt","r") as reader:
+        reader.read()
+
+# except:
+#     print("Somehow i reached this block because try block is failed")
+except Exception as e:
+    print(e)
+
+finally:
+    print("always called no matter try or except block is called")
